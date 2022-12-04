1e10fba2718b5b4e0c3f7df641469cab776a8b72
jdude0822 <jacob.p.nordland@outlook.com>
2022-12-04 17:48:00 -0600

    chapters 4 and 5

--- source/04_hyperspace/linear_regression_04.py
+++ source/04_hyperspace/linear_regression_04.py
@@ -1,5 +1,6 @@
 import numpy as np
 import plot
+import math
 
 def predict(X, w):
     return np.matmul(X, w)
@@ -13,18 +14,23 @@ def gradient(X, Y, w):
 def train(X, Y, iterations, lr):
     w = np.zeros((X.shape[1], 1))
     for i in range(iterations):
-        print("Iteration %4d => Loss: %.6f" % (i, loss(X, Y, w)))
+        loss_val = loss(X, Y, w)
+        if(math.isinf(loss_val)):
+            raise Exception("Value is inf %d iterations" % iterations)
+        if(math.isnan(loss_val)):
+            raise Exception("Value is NAN %d iterations" % iterations)
+        if(i % 5000 == 0):
+            print("Iteration %4d => Loss: %.6f" % (i, loss_val))
         w -= gradient(X, Y, w) * lr
     return w
 
-x1, x2, x3, y = np.loadtxt("pizza_3_vars.txt", skiprows=1, unpack=True)
+x1, x2, x3, y = np.loadtxt("life-expectancy-without-country-names.txt", skiprows=1, unpack=True)
 X = np.column_stack((np.ones(x1.size), x1, x2, x3))
 Y = y.reshape(-1, 1)
 
-w = train(X, Y, iterations = 100000, lr = .001)
+w = train(X, Y, iterations = 10000000, lr = .00005)
 
 print("\nWeights: %s" % w.T)
 print("\nA few predictions:")
 for i in range(5):
     print("X[%d] -> %.4f (label: %d)" % (i, predict(X[i], w), Y[i]))
-#plot.draw_pizza_plot(w, 0)
